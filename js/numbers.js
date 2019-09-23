df15865c63624ae578f4be06a82dffbb42cb77dc
RDee <raghavendradeeduvanu@gmail.com>
2019-09-23 17:56:20 +0530

    First Commit

---
+++ js/numbers.js
@@ -0,0 +1,13 @@
+function initiate(){
+    
+    var hackos = 1393;
+    var onlineCourses = 10;
+    var sportsPrizes = 12;
+    var moviesWatched = 545;
+
+    document.getElementById('hackos').innerHTML = hackos;
+    document.getElementById('onlineCourses').innerHTML = onlineCourses;
+    document.getElementById('sportsPrizes').innerHTML = sportsPrizes;
+    document.getElementById('moviesWatched').innerHTML = moviesWatched;
+    
+}
